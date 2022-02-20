33beff8e2f067863293896dbadb9ec49483b3fd9
aparna <aparnaneelakandhan135@gmail.com>
2022-02-20 07:39:05 +0000

    removed unnecessary settingsssssssdsssssss

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from django.urls import  re_path
+
 from django.conf.urls import url, include
 from app import views
 
@@ -13,7 +13,7 @@ urlpatterns = [
     url('', views.index, name='home'),
 
     # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
+    # re_path(r'^.*\.*', views.pages, name='pages'),
   
 
     # Matches any html file
